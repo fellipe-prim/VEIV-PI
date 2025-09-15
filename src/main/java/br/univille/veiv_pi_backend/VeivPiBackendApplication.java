6b8af46c051b41374acbe6c2e09198907e12c11b
Marioo-Jr <mario.junior23@univille.br>
2025-09-14 22:11:06 -0300

    projeto criado

---
+++ src/main/java/br/univille/veiv_pi_backend/VeivPiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.univille.veiv_pi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeivPiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeivPiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/veiv_pi_backend/VeivPiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.veiv_pi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeivPiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
